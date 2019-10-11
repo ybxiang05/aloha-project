2be7bed9998fdbc8795917395e3a6aec32ed3874
ybxiang05 <brooke.xiang@gmail.com>
2019-10-11 16:17:34 -0700

    subscription alert

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // purchase bubble 
+  // purchase bubble v1 - click cart
 
+  // const cartItems = document.getElementsByClassName("purchase")[0];
+  // const cart = document.getElementsByClassName("add-cart")[0];
+  // let clicks = 0;
+  // cartItems.style.visibility = 'hidden';
+
+  // cart.addEventListener("click", function cart() {
+  //       clicks ++;
+  //     cartItems.innerText = clicks;
+  //     console.log(clicks);
+  //     if (clicks > 0) {
+  //         cartItems.style.visibility = 'visible';
+  //         }
+  // });
+
+  // purchase bubble v2 - lick add-to-cart
+
+  // const addToCart = document.getElementByClassName('add-to-cart')[i];
+
+  const addToCart = document.querySelector('.add-to-cart');
   const cartItems = document.getElementsByClassName("purchase")[0];
-  const cart = document.getElementsByClassName("add-cart")[0];
   let clicks = 0;
   cartItems.style.visibility = 'hidden';
-
-  cart.addEventListener("click", function cart() {
+  addToCart.addEventListener("click", function addToCart() {
         clicks ++;
       cartItems.innerText = clicks;
       console.log(clicks);
       if (clicks > 0) {
           cartItems.style.visibility = 'visible';
-          }
+          };
   });
 
 // flickity carousel
@@ -28,6 +45,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   // email subscription
+
+
+  const userEmail = document.getElementById('user-email');
+  const subscribe = document.getElementsByClassName('email-input')[0];
+
+
+  subscribe.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (userEmail.value == "") {
+      alert("Please input a valid email address")
+    } else {
+      alert("Thank you for subscribing!")
+    }
+      });
+
 
       // if no email input and click or press enter for subscribe, alert("Please submit a valid email address.")
       
