6b2025c68b64b0749968c48dfcd53ecd03a5e1a6
ybxiang05 <brooke.xiang@gmail.com>
2019-10-12 17:59:42 -0700

    final product??

--- js/main.js
+++ js/main.js
@@ -19,58 +19,46 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // const addToCart = document.getElementByClassName('add-to-cart')[i];
 
-  const addToCart = document.querySelector('.add-to-cart');
-  const cartItems = document.getElementsByClassName("purchase")[0];
-  let clicks = 0;
-  cartItems.style.visibility = 'hidden';
-  addToCart.addEventListener("click", function addToCart() {
-        clicks ++;
-      cartItems.innerText = clicks;
-      console.log(clicks);
-      if (clicks > 0) {
-          cartItems.style.visibility = 'visible';
-          };
-  });
+  // flickity carousel
 
-// flickity carousel
-
-  const elem = document.querySelector('.main-carousel');
-  const flkty = new Flickity( elem, {
+  const elem = document.getElementsByClassName("main-carousel")[0];
+  const flkty = new Flickity(elem, {
     // options
-    cellAlign: 'left',
+    cellAlign: "left",
     wrapAround: true,
-    freeScroll: true,
+    freeScroll: false,
     autoPlay: true
   });
 
-
   // email subscription
 
+  const userEmail = document.getElementById("user-email");
+  const form = document.getElementsByClassName("email-input")[0];
 
-  const userEmail = document.getElementById('user-email');
-  const subscribe = document.getElementsByClassName('email-input')[0];
-
-
-  subscribe.addEventListener('submit', function(event) {
+  form.addEventListener("submit", function(event) {
     event.preventDefault();
     if (userEmail.value == "") {
-      alert("Please input a valid email address")
-    } else {
-      alert("Thank you for subscribing!")
+      alert("Please input a valid email address.");
+    } 
+    else {
+      alert("Thank you for subscribing!");
     }
-      });
+  });
 
+  // add to cart
 
-      // if no email input and click or press enter for subscribe, alert("Please submit a valid email address.")
-      
-      // else if incomplete email input and click/enter for subscribe, alert("please entire complete email address.")
-
-      // if complete and click/enter for subscribe, alert ("Thank you for subscribing!")
-      // const submitEmail = document.getElementById("submitted");
-
-      // submitEmail = alert("Thanks for submitting!");
-      // submitEmail.addEventListener("click", {
-      //     if};
-
-
+  const addToCart = document.querySelectorAll(".add-to-cart");
+  const cartItems = document.getElementsByClassName("purchase")[0];
+  let clicks = 0;
+  cartItems.style.visibility = "hidden";
+  for (let i = 0; i < addToCart.length; i++) {
+    addToCart[i].addEventListener("click", function() {
+      clicks++;
+      cartItems.innerText = clicks;
+      console.log(clicks);
+      if (clicks > 0) {
+        cartItems.style.visibility = "visible";
+      }
+    });
+  }
 });
